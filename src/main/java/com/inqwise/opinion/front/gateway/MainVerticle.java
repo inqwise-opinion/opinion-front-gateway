aec975f1c73bd4718621a56e1b597fa625e2220e
alex misyuk <glassfox@gmail.com>
2025-10-30 08:26:55 +0200

    fix: close http server on stop

--- src/main/java/com/inqwise/opinion/front/gateway/MainVerticle.java
+++ src/main/java/com/inqwise/opinion/front/gateway/MainVerticle.java
@@ -3,6 +3,7 @@ package com.inqwise.opinion.front.gateway;
 import com.inqwise.opinion.front.gateway.http.GatewayRouter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,6 +15,7 @@ public class MainVerticle extends AbstractVerticle {
 
   private static final Logger LOGGER = LogManager.getLogger(MainVerticle.class);
   private static final int DEFAULT_HTTP_PORT = 8080;
+  private HttpServer httpServer;
 
   @Override
   public void start(Promise<Void> startPromise) {
@@ -26,13 +28,31 @@ public class MainVerticle extends AbstractVerticle {
       .createHttpServer()
       .requestHandler(router)
       .listen(httpPort)
-      .onSuccess(it -> {
-        LOGGER.info("HTTP server started on port {}", it.actualPort());
+      .onSuccess(server -> {
+        this.httpServer = server;
+        LOGGER.info("HTTP server started on port {}", server.actualPort());
         startPromise.complete();
       })
       .onFailure(error -> {
         LOGGER.error("Failed to start HTTP server on port {}", httpPort, error);
         startPromise.fail(error);
       });
+  }
+
+  @Override
+  public void stop(Promise<Void> stopPromise) {
+    HttpServer server = this.httpServer;
+    if (server == null) {
+      stopPromise.complete();
+      return;
+    }
+
+    server
+      .close()
+      .onSuccess(unused -> {
+        this.httpServer = null;
+        stopPromise.complete();
+      })
+      .onFailure(stopPromise::fail);
   }
 }
